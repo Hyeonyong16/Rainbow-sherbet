cc3376368de2266684574b9e541d2c21b559985d
Breadmannn <you4521@naver.com>
2022-06-18 14:09:24 +0900

    유병훈 폴더이동1번입니다

---
+++ Rainbow_Sherbet/Assets/YBH/Blacknwhite/Assets/ToTitle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToTitle : MonoBehaviour
+{
+    public void Scenetotitle()
+    {
+        SceneManager.LoadScene("Title"); 
+    }
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
