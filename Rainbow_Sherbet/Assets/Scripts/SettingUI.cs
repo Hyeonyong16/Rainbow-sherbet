9b2d6a269c2ef1465deffe6c176edbdc56f47bfa
장민석 <49854389+msjang97@users.noreply.github.com>
2022-06-19 02:14:44 +0900

    씬 수정
    1. 안쓰는씬 삭제 및 수정

---
+++ Rainbow_Sherbet/Assets/Scripts/SettingUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingUI : MonoBehaviour //환경설정 UI ON/OFF
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnSetting()
+    {
+    
+    }
+
+    void OffSetting()
+    { 
+    
+    }
+}
