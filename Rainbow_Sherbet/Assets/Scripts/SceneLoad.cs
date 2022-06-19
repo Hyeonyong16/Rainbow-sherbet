14a6e2d6e047ee5d7cc73a728734c48e977acc54
jwnsqud <jwnsqud@gmail.com>
2022-06-19 21:07:36 +0900

    Update SceneLoad.cs

--- Rainbow_Sherbet/Assets/Scripts/SceneLoad.cs
+++ Rainbow_Sherbet/Assets/Scripts/SceneLoad.cs
@@ -21,49 +21,49 @@ public class SceneLoad : MonoBehaviour
     
     public void LoadStage1()
     {
-        GameManagerUI._instance.colorLimitCount = 2;
+        GameManagerUI._instance.colorLimitCount = 1;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage1");
     }
 
     public void LoadStage2()
     {
-        GameManagerUI._instance.colorLimitCount = 2;
+        GameManagerUI._instance.colorLimitCount = 1;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage2");
     }
 
     public void LoadStage3()
     {
-        GameManagerUI._instance.colorLimitCount = 3;
+        GameManagerUI._instance.colorLimitCount = 2;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage3");
     }
 
     public void LoadStage4()
     {
-        GameManagerUI._instance.colorLimitCount = 4;
+        GameManagerUI._instance.colorLimitCount = 3;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage4");
     }
 
     public void LoadStage5()
     {
-        GameManagerUI._instance.colorLimitCount = 4;
+        GameManagerUI._instance.colorLimitCount = 3;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage5");
     }
 
     public void LoadStage6()
     {
-        GameManagerUI._instance.colorLimitCount = 5;
+        GameManagerUI._instance.colorLimitCount = 4;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage6");
     }
 
     public void LoadStage7()
     {
-        GameManagerUI._instance.colorLimitCount = 5;
+        GameManagerUI._instance.colorLimitCount = 4;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage7");
     }
@@ -71,21 +71,21 @@ public class SceneLoad : MonoBehaviour
 
     public void LoadStage8()
     {
-        GameManagerUI._instance.colorLimitCount = 6;
+        GameManagerUI._instance.colorLimitCount = 5;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage8");
     }
 
     public void LoadStage9()
     {
-        GameManagerUI._instance.colorLimitCount = 7;
+        GameManagerUI._instance.colorLimitCount = 6;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage9");
     }
 
     public void LoadStage10()
     {
-        GameManagerUI._instance.colorLimitCount = 7;
+        GameManagerUI._instance.colorLimitCount = 6;
         GameManagerUI._instance.stageNumStars = 0;
         SceneManager.LoadScene("Stage10");
     }
